6b9ebbcc5ea09f9c62d9fcbdb44090c50c6a03f2
dmitrii kovalev <user@example.com>
2026-04-28 18:57:59 +0700

    add api.py to root

---
+++ api.py
@@ -0,0 +1,14 @@
+"""
+Vercel serverless API.
+"""
+
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write('{"status": "ok"}'.encode("utf-8"))
