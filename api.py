a742edbfa9b3ea54b64e2771eccaf7841e9d56c2
dmitrii kovalev <user@example.com>
2026-04-28 19:00:42 +0700

    minimal vercel.json for api.py

--- api.py
+++ api.py
@@ -1,2 +1,2 @@
 def handler(req, context):
-    return new Response('{"status": "ok"}', status=200, headers={"Content-Type": "application/json"})
+    return new Response('{"status":"ok"}', status=200, headers={"Content-Type": "application/json"})
