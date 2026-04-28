2352886ab2eb554a268fd8c629bf48fdee96f863
dmitrii kovalev <user@example.com>
2026-04-28 20:00:50 +0700

    remove vercel.json

--- api/inplay.py
+++
@@ -1,2 +0,0 @@
-async def handler(request):
-    return Response('{"status":"ok","v":7}', status=200, headers={"Content-Type": "application/json"})
--- api/webhook.py
+++
@@ -1,2 +0,0 @@
-async def handler(request):
-    return Response('{"ok":true,"v":2}', status=200, headers={"Content-Type": "application/json"})
