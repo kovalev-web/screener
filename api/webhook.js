f2116fbbfca4674d271abbb2670462e0086d5097
dmitrii kovalev <user@example.com>
2026-04-28 20:44:15 +0700

    export default function

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, context) {
+export default function handler(req, context) {
   return new Response('{"ok":true}', {
     status: 200,
     headers: {"Content-Type": "application/json"}
