721029e717dc015920546ff3b60a3d8410c8869b
dmitrii kovalev <user@example.com>
2026-04-28 19:48:49 +0700

    v7: handler request

--- api/inplay.py
+++ api/inplay.py
@@ -1,2 +1,2 @@
 async def handler(request):
-    return Response('{"status":"ok","v":6}', status=200, headers={"Content-Type": "application/json"})
+    return Response('{"status":"ok","v":7}', status=200, headers={"Content-Type": "application/json"})
